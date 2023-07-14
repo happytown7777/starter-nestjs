1d1ad5888ac6ea809188703b82f3803af7716532
SuperDev0123 <superdev1230@outlook.com>
2023-07-14 15:47:31 -0300

    db setup

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  //   origin: 'http://localhost:3000',
+  //   credentials: true,
+  // });
+  // await app.listen(8000);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: 'http://localhost:3000',
+    origin: 'http://haruhana.com/',
     credentials: true,
   });
-  await app.listen(8000);
+  await app.listen(3000);
 }
 bootstrap();
