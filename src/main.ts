51cd659ba153e6195be942b2c28dd1974cae5ada
SuperDev0123 <superdev1230@outlook.com>
2023-07-16 14:01:22 -0300

    f

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   methods: ["GET", "POST", "PUT", "DELETE"],
-  //   origin: 'http://localhost:3000',
-  //   credentials: true,
-  // });
-  // await app.listen(8000);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: 'https://haruhana-happytown.com/',
+    origin: true,
     credentials: true,
   });
-  await app.listen(80);
+  // await app.listen(8000);
+  await app.listen(3000);
 }
 bootstrap();
