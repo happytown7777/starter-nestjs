b166d5e5228a15910f81cd74abe47c7f916fb515
SuperDev0123 <superdev1230@outlook.com>
2023-08-28 15:10:24 -0300

    fix: register route issue

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: false,
-    // credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
   });
+  // app.enableCors({
+  //   // methods: ["GET", "POST", "PUT", "DELETE"],
+  //   origin: false,
+  //   // credentials: true,
+  // });
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
