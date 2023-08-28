17ffd9c0ce48461ded3be85550b456c36c5afab8
SuperDev0123 <superdev1230@outlook.com>
2023-08-28 09:51:05 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    // origin: true,
-    // credentials: true,
-  });
+  // app.enableCors({
+  //   // methods: ["GET", "POST", "PUT", "DELETE"],
+  //   // origin: true,
+  //   // credentials: true,
+  // });
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
