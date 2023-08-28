2985c862f4241155db55c18e0a5d57860675b0a2
SuperDev0123 <superdev1230@outlook.com>
2023-08-28 16:05:23 -0300

    fix: register route issue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as Express from 'express';
+import * as cors from 'cors';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = Express();
+server.use(cors());
+server.get('/', (req, res) => res.send('ok'));
+server.get('/_ah/health', (req, res) => res.send('ok'));
+server.get('/_ah/start', (req, res) => res.send('ok'));
+
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    // methods: ["GET", "POST", "PUT", "DELETE"],
     origin: false,
     // credentials: true,
   });
