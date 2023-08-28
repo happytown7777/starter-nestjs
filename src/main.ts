18bc00ab2549399966a1e3db16893f1a649fe666
SuperDev0123 <superdev1230@outlook.com>
2023-08-28 10:10:39 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: [
-      'http://localhost:3000',
-      'https://haruhana-happytown.com',
-    ],
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
   });
+  // app.enableCors({
+  //   methods: ["GET", "POST", "PUT", "DELETE"],
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://haruhana-happytown.com',
+  //   ],
+  //   credentials: true,
+  // });
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
